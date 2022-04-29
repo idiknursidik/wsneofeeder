0cbe8506761d0910529e1cafa47c076cdbe3c87c
idiknursidik <42262063+idiknursidik@users.noreply.github.com>
2022-04-29 21:33:36 +0700

    Update index.blade.php

--- resources/views/dictionary/index.blade.php
+++ resources/views/dictionary/index.blade.php
@@ -5,7 +5,7 @@
     @csrf
     <input type="text" name="dictionary" class="form-control" placeholeder="masukan nama fungsi">
     <br>
-    <button type="submit" class="btn btn-success">Cek Kamus</button>
+    <button type="submit" id="btnSubmit_form-cek" class="btn btn-success">Cek Kamus</button>
 </form>
 <hr> 
 <div id="resultcontent">Masukan nama fungsi pada form input di atas.</div>
@@ -18,18 +18,28 @@ $.ajaxSetup({
 $(function(){
     $("#form-cek").on("submit",function(e){
         e.preventDefault();
+        var id = $(this).attr("id");
+        var btnHtml = $("#btnSubmit_"+id+"").html();
         var dString = $(this).serialize();
         var url =  $(this).attr("action");
         $.ajax({
            url:url,
            method:'POST',
            data:dString,
-           success:function(response){
+            beforeSend:function(){
+				$("#btnSubmit_"+id+"").prop("disabled",true);
+				$("#btnSubmit_"+id+"").html("<span class='spinner-grow spinner-grow-sm' role='status' aria-hidden='true'></span> Loading...");			
+			},
+			complete:function(){
+				$("#btnSubmit_"+id+"").prop("disabled",false);
+				$("#btnSubmit_"+id+"").html(btnHtml);	
+			},
+            success:function(response){
               $("#resultcontent").html(response);
-           },
-           error:function(error){
-              console.log(error)
-           }
+            },
+            error:function(xhr,ajaxOptions,thrownError){
+				alert(xhr.status+"\n"+xhr.responseText+"\n"+thrownError);				
+			}
         });
         return false;
     })
