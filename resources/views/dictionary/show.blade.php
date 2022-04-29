4259c42dc957085ff688649d2e6788a4525835a8
idiknursidik <42262063+idiknursidik@users.noreply.github.com>
2022-04-29 21:26:08 +0700

    Update show.blade.php

--- resources/views/dictionary/show.blade.php
+++ resources/views/dictionary/show.blade.php
@@ -1,3 +1,23 @@
+<h3>Request</h3>
+<table class="table">
+    <thead><tr><th>No</th><th>Nama Field</th><th>Type</th><th>Keterangan</th></tr></thead>
+    <tbody>
+        @php $no=0; @endphp
+        @foreach($data->data->request as $key=>$val)
+            @if(!in_array($key,array('error_code','error_desc','data','token')))
+            @php $no++ @endphp
+            <tr>
+                <td>{{ $no  }}</td>
+                <td>{{ str_replace(array('record[',']','key['),array(""),$key) }}</td>
+                <td>@if(!empty($val->type)) {{ $val->type }} @else @endif</td>
+                <td>@if(!empty($val->keterangan)) {{ $val->keterangan }} @else @endif</td>
+            </tr>
+            @endif
+        @endforeach
+    </tbody>
+</table>
+
+<h3>Response</h3>
 <table class="table">
     <thead><tr><th>No</th><th>Nama Field</th><th>Keterangan</th></tr></thead>
     <tbody>
